c2b75aaf5944f83a62102574645a033d70988a77
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-11-02 10:20:44 -0500

    commit tree 9225a7a3667c0077786dc1731852bf1c884a3ea0

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School
+  attr_reader :name
+  attr_accessor :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << student_name
+  end
+
+  def grade(grade_level)
+    @roster[grade_level]
+  end
+
+  def sort
+    @roster.each { |_grade_level, student| student.sort! }
+  end
+end
